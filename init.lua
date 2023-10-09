e6e8680342bbf010512cf58b324689210ced2403
phurinutw <phurinut.wara@gmail.com>
2023-10-09 09:29:35 +0700

    ✨ Add relative path to lualine

--- init.lua
+++ init.lua
@@ -174,6 +174,14 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          },
+        },
+      },
     },
   },
 
