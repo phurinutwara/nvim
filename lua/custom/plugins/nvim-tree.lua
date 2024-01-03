1c0c62da527d59c418539d4a8cd72f9950fefc0c
Phurinut Waranich <phurinut.wara@gmail.com>
2024-01-03 18:08:44 +0700

    add smart nvim-tree toggling

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -5,9 +5,6 @@ return {
 	requires = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	keys = {
-		{ "<leader>b", ":NvimTreeToggle<CR>", "Toggle sidebar" }
-	},
 	config = function()
 		-- disable netrw at the very start of your init.lua
 		vim.g.loaded_netrw = 1
@@ -15,6 +12,21 @@ return {
 
 		-- set termguicolors to enable highlight groups
 		vim.opt.termguicolors = true
+
+
+		-- Smart nvim-tree toggling
+		-- See more: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#smart-nvim-tree-toggling
+		local nvimTreeFocusOrToggle = function()
+			local nvimTree = require("nvim-tree.api")
+			local currentBuf = vim.api.nvim_get_current_buf()
+			local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
+			if currentBufFt == "NvimTree" then
+				nvimTree.tree.toggle()
+			else
+				nvimTree.tree.focus()
+			end
+		end
+		vim.keymap.set("n", "<leader>b", nvimTreeFocusOrToggle, { desc = "Toggle sidebar" })
 
 		require("nvim-tree").setup({
 			disable_netrw = true,
