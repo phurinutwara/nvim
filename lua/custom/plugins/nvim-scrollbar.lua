1a9eb800d3443fe09b105e77cf41b76ca5e16138
Phurinut Waranich <phurinut.wara@gmail.com>
2024-01-05 17:19:12 +0700

    add nvim-scrollbar

---
+++ lua/custom/plugins/nvim-scrollbar.lua
@@ -0,0 +1,11 @@
+return {
+	"petertriho/nvim-scrollbar",
+	config = function()
+		local colors = require("catppuccin.palettes").get_palette("mocha")
+		require("scrollbar").setup({
+			handle = {
+				color = colors.surface2
+			},
+		})
+	end,
+}
