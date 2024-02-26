4707c09be464a6a9b670d5b5d7c652c69def73fc
Phurinut Waranich <phurinut.wara@gmail.com>
2024-02-27 02:09:42 +0700

    add pattern only for files in (better-whitespace)

--- lua/custom/plugins/better-whitespace.lua
+++ lua/custom/plugins/better-whitespace.lua
@@ -7,7 +7,7 @@ return {
       local colors = require('catppuccin.palettes').get_palette 'mocha'
       vim.api.nvim_set_hl(0, 'ExtraWhitespace', { bg = colors.red })
       vim.api.nvim_create_autocmd({ 'FileType' }, {
-        pattern = '*',
+        pattern = '*.*',
         command = 'EnableWhitespace',
       })
     end,
