af719fd39a25dcddb163fb2ed71929b0b89bd223
Phurinut Waranich <phurinut.wara@gmail.com>
2024-02-26 14:49:39 +0700

    restructuring dap with modular by programming language

--- lua/custom/plugins/dap/init.lua
+++ lua/custom/plugins/dap/init.lua
@@ -2,11 +2,9 @@ return {
   {
     'rcarriga/nvim-dap-ui',
     event = 'VeryLazy',
-    dependencies = {
-      'mfussenegger/nvim-dap',
-    },
+    dependencies = { 'mfussenegger/nvim-dap' },
     keys = {
-      { '<leader>dbt', ":lua require('dapui').toggle()<CR>", desc = 'DAP: Toggle UI' },
+      { '<leader>dbg', ":lua require('dapui').toggle()<CR>", desc = 'DAP: Toggle UI' },
       { '<leader>dbb', ':DapToggleBreakpoint<CR>', desc = 'DAP: Toggle Breakpoint' },
       { '<leader>dbc', ':DapContinue<CR>', desc = 'DAP: Continue' },
       { '<leader>dbr', ":lua require('dapui').open({reset = true})<CR>", desc = 'DAP: Reset UI' },
@@ -20,29 +18,7 @@ return {
       }
       require('dapui').setup()
       vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-
-      local dap = require 'dap'
-      dap.adapters['pwa-node'] = {
-        type = 'server',
-        host = '127.0.0.1',
-        port = 8123,
-        executable = {
-          command = 'js-debug-adapter',
-        },
-      }
-
-      for _, language in ipairs { 'typescript', 'javascript' } do
-        dap.configurations[language] = {
-          {
-            type = 'pwa-node',
-            request = 'launch',
-            name = 'Launch file',
-            program = '${file}',
-            cwd = '${workspaceFolder}',
-            runtimeExecutable = 'node',
-          },
-        }
-      end
     end,
   },
+  { import = 'custom.plugins.dap.javascript' },
 }
