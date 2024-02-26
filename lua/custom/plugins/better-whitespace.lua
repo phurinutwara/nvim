9d7840c3c7806f23f64f8208368ab451a2378acb
Phurinut Waranich <phurinut.wara@gmail.com>
2024-02-26 15:55:41 +0700

    add better-whitespace plugin

---
+++ lua/custom/plugins/better-whitespace.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'ntpeters/vim-better-whitespace',
+    event = 'VeryLazy',
+    name = 'better-whitespace',
+    config = function()
+      local colors = require('catppuccin.palettes').get_palette 'mocha'
+      vim.api.nvim_set_hl(0, 'ExtraWhitespace', { bg = colors.red })
+      vim.api.nvim_create_autocmd({ 'FileType' }, {
+        pattern = '*',
+        command = 'EnableWhitespace',
+      })
+    end,
+  },
+}
