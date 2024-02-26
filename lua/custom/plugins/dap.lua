45b2657c49d40e5a1cd4271f1ddfbeb608e7d6e4
Phurinut Waranich <phurinut.wara@gmail.com>
2024-02-26 07:51:24 +0700

    remap DAP keys

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -6,12 +6,15 @@ return {
       'mfussenegger/nvim-dap',
     },
     keys = {
-      { '<leader>dt', ":lua require('dapui').toggle()<CR>", desc = 'DAP: Toggle UI' },
-      { '<leader>db', ':DapToggleBreakpoint<CR>', desc = 'DAP: Toggle Breakpoint' },
-      { '<leader>dc', ':DapContinue<CR>', desc = 'DAP: Continue' },
-      { '<leader>dr', ":lua require('dapui').open({reset = true})<CR>", desc = 'DAP: Reset UI' },
+      { '<leader>dbt', ":lua require('dapui').toggle()<CR>", desc = 'DAP: Toggle UI' },
+      { '<leader>dbb', ':DapToggleBreakpoint<CR>', desc = 'DAP: Toggle Breakpoint' },
+      { '<leader>dbc', ':DapContinue<CR>', desc = 'DAP: Continue' },
+      { '<leader>dbr', ":lua require('dapui').open({reset = true})<CR>", desc = 'DAP: Reset UI' },
     },
     config = function()
+      require('which-key').register {
+        ['<leader>db'] = { name = '[D]e[B]ugger', _ = 'which_key_ignore' },
+      }
       require('neodev').setup {
         library = { plugins = { 'nvim-dap-ui' }, types = true },
       }
