5d7b0291c76003c3c5aa54fd5931410a976b7a4d
Phurinut Waranich <phurinut.wara@gmail.com>
2024-02-27 14:36:07 +0700

    set default nvim-tree size to 50 cols

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,41 +1,43 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	requires = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		-- disable netrw at the very start of your init.lua
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  requires = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-		-- set termguicolors to enable highlight groups
-		vim.opt.termguicolors = true
+    -- set termguicolors to enable highlight groups
+    vim.opt.termguicolors = true
 
+    -- Smart nvim-tree toggling
+    -- See more: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#smart-nvim-tree-toggling
+    local nvimTreeFocusOrToggle = function()
+      local nvimTree = require 'nvim-tree.api'
+      local currentBuf = vim.api.nvim_get_current_buf()
+      local currentBufFt = vim.api.nvim_get_option_value('filetype', { buf = currentBuf })
+      if currentBufFt == 'NvimTree' then
+        nvimTree.tree.toggle()
+      else
+        nvimTree.tree.focus()
+      end
+    end
+    vim.keymap.set('n', '<leader>b', nvimTreeFocusOrToggle, { desc = 'Toggle sidebar' })
 
-		-- Smart nvim-tree toggling
-		-- See more: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#smart-nvim-tree-toggling
-		local nvimTreeFocusOrToggle = function()
-			local nvimTree = require("nvim-tree.api")
-			local currentBuf = vim.api.nvim_get_current_buf()
-			local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
-			if currentBufFt == "NvimTree" then
-				nvimTree.tree.toggle()
-			else
-				nvimTree.tree.focus()
-			end
-		end
-		vim.keymap.set("n", "<leader>b", nvimTreeFocusOrToggle, { desc = "Toggle sidebar" })
-
-		require("nvim-tree").setup({
-			disable_netrw = true,
-			update_focused_file = {
-				enable = true,
-			},
-			filters = {
-				git_ignored = false,
-			},
-		})
-	end,
+    require('nvim-tree').setup {
+      disable_netrw = true,
+      update_focused_file = {
+        enable = true,
+      },
+      filters = {
+        git_ignored = false,
+      },
+      view = {
+        width = 50,
+      },
+    }
+  end,
 }
