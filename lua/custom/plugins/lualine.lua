aa6a3b631f3f2561785bcca9462625c8720f81ab
Phurinut Waranich <phurinut.wara@gmail.com>
2024-02-29 14:46:31 +0700

    lualine: add ignore_focus setup on ( dap ) plugin

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -19,6 +19,14 @@ return {
             left = '',
             right = '',
           },
+          ignore_focus = {
+            'dapui_watches',
+            'dapui_breakpoints',
+            'dapui_scopes',
+            'dapui_console',
+            'dapui_stacks',
+            'dap-repl',
+          },
         },
       }
 
