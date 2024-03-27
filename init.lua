57eec82e8bbcf5f5292ccfcd88d0aa4b0af9d243
Phurinut Waranich <phurinut.wara@gmail.com>
2024-03-27 18:09:40 +0700

    git.lua: extract all good old-fashioned git plugins to new file

--- init.lua
+++ init.lua
@@ -236,10 +236,6 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
