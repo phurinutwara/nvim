bc9db985155d299324bfd984caabb3f905eb6980
Phurinut Waranich <phurinut.wara@gmail.com>
2024-05-20 14:32:35 +0700

    vim-go: use `fatih/vim-go` instead of `ray-x/go.nvim` for better experiences

--- lua/custom/plugins/languages/go/init.lua
+++ lua/custom/plugins/languages/go/init.lua
@@ -1,26 +1,29 @@
 return {
   {
-    'ray-x/go.nvim',
-    dependencies = { -- optional packages
-      'ray-x/guihua.lua',
+    'fatih/vim-go',
+    dependencies = {
       'neovim/nvim-lspconfig',
-      'nvim-treesitter/nvim-treesitter',
     },
+    ft = { 'go', 'gomod', 'gosum' },
     config = function()
-      -- setup your go.nvim
-      -- make sure lsp_cfg is disabled
-      require('mason').setup()
-      require('mason-lspconfig').setup()
-      require('go').setup {
-        lsp_cfg = false,
-        -- other setups...
-      }
-      local cfg = require('go.lsp').config() -- config() return the go.nvim gopls setup
+      local lspconfig = require 'lspconfig'
+      vim.lsp.inlay_hint.enable(true)
 
-      require('lspconfig').gopls.setup(cfg)
+      lspconfig.gopls.setup {
+        settings = {
+          gopls = {
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+          },
+        },
+      }
     end,
-    event = { 'CmdlineEnter' },
-    ft = { 'go', 'gomod' },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
