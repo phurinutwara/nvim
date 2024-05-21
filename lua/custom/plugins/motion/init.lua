29f81010903f0fa08045ab016fcd008bba9a1aa5
Phurinut Waranich <phurinut.wara@gmail.com>
2024-05-22 02:03:23 +0700

    nvim-treesitter-textobjects: add this plugin to use parameter swap feature

---
+++ lua/custom/plugins/motion/init.lua
@@ -0,0 +1,3 @@
+return {
+  import = 'custom.plugins.motion',
+}
---
+++ lua/custom/plugins/motion/swap.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        textobjects = {
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            },
+          },
+        },
+      }
+    end,
+  },
+}
