4476e086bdb1f6b77ee6a97681a70a7f867c0d8f
Phurinut Waranich <phurinut.wara@gmail.com>
2024-05-28 12:02:21 +0700

    inc-rename.lua: configured to add current word picker while renaming

--- lua/custom/plugins/lsp/inc-rename.lua
+++ lua/custom/plugins/lsp/inc-rename.lua
@@ -3,7 +3,8 @@ return {
     'smjonas/inc-rename.nvim',
     config = function()
       require('inc_rename').setup {}
-      vim.keymap.set('n', '<leader>rn', ':IncRename ', { desc = 'LSP: Increment [R]e[n]ame' })
+      vim.keymap.set('n', '<leader>rn', [[:IncRename <C-r><C-w>]], { desc = 'LSP: Increment [R]e[n]ame' })
+      vim.keymap.set('n', '<leader>rN', ':IncRename ', { desc = 'LSP: Increment [R]e[n]ame' })
     end,
   },
 }
