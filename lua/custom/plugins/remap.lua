c1301ef49cdf9ac1226f4f19961386b08a98da01
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-03 17:27:37 +0700

    configs: config plugin ( toggleterm.lua, trouble.lua )

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -1,74 +1,74 @@
 local vmap = function(keys, func, desc)
-	if desc then
-		desc = 'ThePrimeagen: ' .. desc
-	end
+  if desc then
+    desc = 'ThePrimeagen: ' .. desc
+  end
 
-	vim.keymap.set('v', keys, func, { buffer = bufnr, desc = desc })
+  vim.keymap.set('v', keys, func, { buffer = bufnr, desc = desc })
 end
 
 local nmap = function(keys, func, desc)
-	if desc then
-		desc = 'ThePrimeagen: ' .. desc
-	end
+  if desc then
+    desc = 'ThePrimeagen: ' .. desc
+  end
 
-	vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 end
 
 local xmap = function(keys, func, desc)
-	if desc then
-		desc = 'ThePrimeagen: ' .. desc
-	end
+  if desc then
+    desc = 'ThePrimeagen: ' .. desc
+  end
 
-	vim.keymap.set('x', keys, func, { buffer = bufnr, desc = desc })
+  vim.keymap.set('x', keys, func, { buffer = bufnr, desc = desc })
 end
 
 local imap = function(keys, func, desc)
-	if desc then
-		desc = 'ThePrimeagen: ' .. desc
-	end
+  if desc then
+    desc = 'ThePrimeagen: ' .. desc
+  end
 
-	vim.keymap.set('i', keys, func, { buffer = bufnr, desc = desc })
+  vim.keymap.set('i', keys, func, { buffer = bufnr, desc = desc })
 end
 
-vmap("J", ":m '>+1<CR>gv=gv", "Move selection downward")
-vmap("K", ":m '<-2<CR>gv=gv", "Move selection upward")
+vmap('J', ":m '>+1<CR>gv=gv", 'Move selection downward')
+vmap('K', ":m '<-2<CR>gv=gv", 'Move selection upward')
 
-nmap("J", "mzJ`z", "[J]oin line with steady cursor")
-nmap("<C-d>", "<C-d>zz", "Move [D]own and center-screen")
-nmap("<C-u>", "<C-u>zz", "Move [U]p and center-screen")
-nmap("n", "nzzzv", "[N]ext search and center-screen")
-nmap("N", "Nzzzv", "Previous search and center-screen")
+nmap('J', 'mzJ`z', '[J]oin line with steady cursor')
+nmap('<C-d>', '<C-d>zz', 'Move [D]own and center-screen')
+nmap('<C-u>', '<C-u>zz', 'Move [U]p and center-screen')
+nmap('n', 'nzzzv', '[N]ext search and center-screen')
+nmap('N', 'Nzzzv', 'Previous search and center-screen')
 
 -- greatest remap ever
-xmap("<leader>p", [["_dP]], "[P]aste without lose yank")
+xmap('<leader>p', [["_dP]], '[P]aste without lose yank')
 
 -- next greatest remap ever : asbjornHaland
-nmap("<leader>y", [["+y]], "[Y]ank to clipboard")
-vmap("<leader>y", [["+y]], "[Y]ank to clipboard")
-nmap("<leader>Y", [["+Y]], "[Y]ank line to clipboard")
+nmap('<leader>y', [["+y]], '[Y]ank to clipboard')
+vmap('<leader>y', [["+y]], '[Y]ank to clipboard')
+nmap('<leader>Y', [["+Y]], '[Y]ank line to clipboard')
 
-nmap("<leader>d", [["_d]], "[D]elete without lose yank")
-vmap("<leader>d", [["_d]], "[D]elete without lose yank")
+nmap('<leader>d', [["_d]], '[D]elete without lose yank')
+vmap('<leader>d', [["_d]], '[D]elete without lose yank')
 
 -- make Ctrl-C more useful
-imap("<C-c>", "<Esc>", "Quit insertion (ESC)")
+imap('<C-c>', '<Esc>', 'Quit insertion (ESC)')
 
-nmap("Q", "<nop>")
-nmap("<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-nmap("<leader>f", vim.lsp.buf.format, "[F]ormat with LSP")
-nmap("<leader>nf", "<cmd>Neoformat<CR>", "[N]eo[F]ormat")
-vmap("<leader>nf", ":'<,'>Neoformat<CR>", "[N]eo[F]ormat Selection")
+nmap('Q', '<nop>')
+nmap('<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+nmap('<leader>f', vim.lsp.buf.format, '[F]ormat with LSP')
+nmap('<leader>nf', '<cmd>Neoformat<CR>', '[N]eo[F]ormat')
+vmap('<leader>nf', ":'<,'>Neoformat<CR>", '[N]eo[F]ormat Selection')
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 -- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-nmap("<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Fast [S]earch in buffer")
-nmap("<leader>x", "<cmd>!chmod +x %<CR>", "Grant e[X]ecutable")
+nmap('<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], 'Fast [S]earch in buffer')
+nmap('<leader>X', '<cmd>!chmod +x %<CR>', 'Grant e[X]ecutable')
 
 -- Vim-fugitive
-nmap("<leader>gs", vim.cmd.Git, "Open Vim-fugitive")
+nmap('<leader>gs', vim.cmd.Git, 'Open Vim-fugitive')
 
 -- Tabs (!DEPRECATED, mapped by bufferline.lua instead)
 -- nmap("te", ":tabedit ", "Open new tab")
--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,12 +1,39 @@
 return {
   {
     'folke/trouble.nvim',
-    config = function()
-      require('trouble').setup {}
-    end,
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
     keys = {
-      { '<leader>t ', ':Trouble<CR>', desc = '[T]rouble Menu' },
-      { '<leader>td', ':Trouble diagnostics<CR>', desc = '[T]rouble [D]iagnostic' },
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
     },
   },
 }
