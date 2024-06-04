8b3cc2aaee04c2762619ac7e8e33c866bf82488a
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-05 04:40:02 +0700

    toggleterm.lua: modify config by pressing <esc> twices to unfocus term

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -10,10 +10,11 @@ return {
       local Terminal = require('toggleterm.terminal').Terminal
       Terminal:new { hidden = true }
 
+      -- See more: https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#terminal-window-mappings
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
+        -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
         vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
         vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
