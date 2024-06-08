bc3ffebee84b0cf5301ee0d7e93f790c3d9505ec
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-08 20:54:47 +0700

    wilder.lua: make python remote plugin available

--- lua/custom/plugins/wilder.lua
+++ lua/custom/plugins/wilder.lua
@@ -1,10 +1,18 @@
 return {
   {
     'gelguy/wilder.nvim',
+    dependencies = {
+      { 'roxma/vim-hug-neovim-rpc' },
+      {
+        'roxma/nvim-yarp',
+        build = 'cat requirements.txt | xargs python3 -m pip install ',
+      },
+    },
     config = function()
       require('wilder').setup {
         modes = { ':', '/', '?' },
       }
+      -- NOTE: Don't forget to run :UpdateRemotePlugins
     end,
   },
 }
