e73847448b8ad1318850699c28028577ff291bbb
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-08 16:20:09 +0700

    codesnap.lua: add this plugin to screenshot the code 🖥️

---
+++ lua/custom/plugins/codesnap.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'mistricky/codesnap.nvim',
+    build = 'make build_generator',
+    keys = {
+      { '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'Save selected code snapshot into clipboard' },
+      { '<leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+    },
+    opts = {
+      save_path = '~/Pictures',
+
+      has_breadcrumbs = true,
+      has_line_number = false,
+      mac_window_bar = true,
+
+      breadcrumbs_separator = '/',
+      title = 'CodeSnap.nvim',
+      code_font_family = 'CaskaydiaCove Nerd Font',
+      watermark = 'CodeSnap.nvim',
+      watermark_font_family = 'Pacifico',
+      bg_theme = 'bamboo',
+
+      min_width = 0,
+    },
+  },
+}
