ca5e74dad0510d075bf9ebbba801d609471aa538
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-19 15:58:57 +0700

    markdown-preview.lua: add shortcut to view docs of mermaid and example

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,9 +1,20 @@
 return {
   {
     'iamcco/markdown-preview.nvim',
-    event = 'VeryLazy',
-    config = function()
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
       vim.fn['mkdp#util#install']()
     end,
   },
 }
+
+-- To use with mermaid(https://github.com/mermaid-js/mermaid)
+-- use code blocks with tag mermaid, e.g.
+--
+-- ``` mermaid
+-- ...your code here...
+--
+-- ```
+--
+-- See more: https://github.com/iamcco/markdown-preview.nvim?tab=readme-ov-file#custom-examples
