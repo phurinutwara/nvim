002eb336afe1523f9595f3c039109f0d0f7cea98
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-20 13:26:37 +0700

    nvim-treesitter: extract treesitter plugin from init.lua

--- init.lua
+++ init.lua
@@ -871,7 +871,7 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [']quote
       --  - ci'  - [C]hange [I]nside [']quote
-      -- require('mini.ai').setup { n_lines = 500 }
+      require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
--- lua/custom/plugins/motion/nvim-surround.lua
+++ lua/custom/plugins/motion/nvim-surround.lua
@@ -1,12 +1,21 @@
 return {
   {
     'kylechui/nvim-surround',
+    enabled = true,
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     config = function()
       require('nvim-surround').setup {
         -- Configuration here, or leave empty to use defaults
       }
     end,
+  },
+  {
+    'tpope/vim-surround',
+    enabled = false,
   },
 }
