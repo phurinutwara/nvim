0c1ff62a715df615e6d43fc088b83eceefab7832
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-22 04:26:17 +0700

    nvim-notify: add silent keymap

--- lua/custom/plugins/ui/nvim-notify.lua
+++ lua/custom/plugins/ui/nvim-notify.lua
@@ -4,6 +4,16 @@ return {
     dependencies = {
       'nvim-telescope/telescope.nvim',
     },
+    lazy = false,
+    keys = {
+      {
+        '<leader>rs',
+        function()
+          require('notify').dismiss {}
+        end,
+        desc = '[S]ilent notify (dismiss)',
+      },
+    },
     config = function()
       require('notify').setup {
         fps = 120,
