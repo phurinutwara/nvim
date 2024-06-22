38655462efc2042be7bf9743e227587686d903ae
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-22 07:10:52 +0700

    firenvim: add references for font config

--- lua/custom/plugins/nvim-essential/firenvim.lua
+++ lua/custom/plugins/nvim-essential/firenvim.lua
@@ -54,11 +54,11 @@ return {
         end,
       })
 
-      -- set font for firenvim instance: https://github.com/glacambre/firenvim/issues/110
+      -- set font for firenvim instance: https://github.com/glacambre/firenvim/issues/110#issuecomment-550137400
       vim.api.nvim_create_autocmd({ 'UIEnter' }, {
         callback = function(e)
           vim.notify('nvim instance stated with firenvim', vim.log.levels.INFO)
-          vim.cmd [[set guifont=JetBrainsMono\ Nerd\ Font\ Mono:h10]] -- https://github.com/glacambre/firenvim/issues/110#issuecomment-550137400
+          vim.cmd [[set guifont=JetBrainsMono\ Nerd\ Font\ Mono:h10]] -- https://forums.freebsd.org/threads/vim-set-guifont.79219/
           vim.g.format_is_enabled = false
           vim.g.go_fmt_autosave = 0
         end,
