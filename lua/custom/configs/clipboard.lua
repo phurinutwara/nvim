7c1a2b2a37e76d61e39e981da47ac9eeb6f79656
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-25 22:57:52 +0700

    clipboard.lua: made WSL able to copy-paste clipboard
    but the performance was slow due to WSL2 Filesystem
    this need to be fixed, solutions are on the file

--- lua/custom/configs/clipboard.lua
+++ lua/custom/configs/clipboard.lua
@@ -6,31 +6,35 @@ return {
       'rcarriga/nvim-notify',
     },
     config = function()
-      local TMUX = os.getenv 'TMUX'
       local OSTYPE = (vim.api.nvim_exec2([[silent !uname --kernel-release]], { output = true }))
 
-      local function no_paste(reg)
-        return function(lines)
-          vim.notify_once('disable paste from OSC52, using " register!', vim.log.levels.WARN)
-        end
-      end
+      if string.match(OSTYPE.output, '(WSL)') == 'WSL' then
+        vim.notify_once('use wsl as clipboard provider (:h clipboard-wsl)', vim.log.levels.INFO)
+        vim.cmd [[
+          let g:clipboard = {
+          \   'name': 'WslClipboard',
+          \   'copy': {
+          \      '+': '/mnt/c/WINDOWS/system32/clip.exe',
+          \    },
+          \   'paste': {
+          \      '+': '/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0//powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+          \   },
+          \   'cache_enabled': 0,
+          \ }
+        ]]
 
-      if TMUX == nil and string.match(OSTYPE.output, '(WSL)') == 'WSL' then
-        vim.notify_once('use osc52 as clipboard provider', vim.log.levels.INFO)
-        vim.g.clipboard = {
-          name = 'OSC 52',
-          copy = {
-            ['+'] = require('vim.ui.clipboard.osc52').copy '+',
-            ['*'] = require('vim.ui.clipboard.osc52').copy '*',
-          },
-          paste = {
-            ['+'] = no_paste '+', -- Pasting disabled
-            ['*'] = no_paste '*', -- Pasting disabled
-
-            -- ['+'] = require('vim.ui.clipboard.osc52').paste '+', -- TODO: write wezterm.lua script to return clipboard when nvim request
-            -- ['*'] = require('vim.ui.clipboard.osc52').paste '*', -- TODO: write wezterm.lua script to return clipboard when nvim request
-          },
-        }
+        -- TODO: FIX SLOW PERFORMANCE USING /mnt/c on paste (copy is fine) by this:
+        --       https://stackoverflow.com/a/76314967/15029223
+        --
+        -- NOTE: use `time [cmd] ` to meature the time, now it's 0.3s delay
+        --       0.00user 0.00system 0:00.31elapsed 0%CPU (0avgtext+0avgdata 1876maxresident)k
+        --       0inputs+0outputs (0major+322minor)pagefaults 0swaps
+        --       0.00user 0.00system 0:00.28elapsed 0%CPU (0avgtext+0avgdata 1876maxresident)k
+        --       0inputs+0outputs (0major+322minor)pagefaults 0swaps
+        --
+        -- NOTE: '+' (unnamedplus): For Windows and macOS (system clipboard)
+        --       '*' (unnamed)    : Exclusively for Linux (it's selection clipboard)
+        --       See more: https://stackoverflow.com/a/30691754/15029223
       end
     end,
   },
