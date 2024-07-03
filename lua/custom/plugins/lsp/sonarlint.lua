16300bb34798fa11648a113edd2f12abd726b881
Phurinut Waranich <phurinut.wara@gmail.com>
2024-07-03 12:06:21 +0700

    sonarlint.lua: add sonarlint to plugin for codescan

---
+++ lua/custom/plugins/lsp/sonarlint.lua
@@ -0,0 +1,27 @@
+return {
+  url = 'https://gitlab.com/schrieveslaach/sonarlint.nvim',
+  config = function()
+    require('sonarlint').setup {
+      server = {
+        cmd = {
+          'sonarlint-language-server',
+          -- Ensure that sonarlint-language-server uses stdio channel
+          '-stdio',
+          '-analyzers',
+          -- paths to the analyzers you need, using those for python and java in this example
+          -- vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarpython.jar',
+          -- vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarcfamily.jar',
+          -- vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarjava.jar',
+          vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarjs.jar',
+        },
+      },
+      filetypes = {
+        -- Tested and working
+        -- 'python',
+        -- 'cpp',
+        -- 'java',
+        'js',
+      },
+    }
+  end,
+}
