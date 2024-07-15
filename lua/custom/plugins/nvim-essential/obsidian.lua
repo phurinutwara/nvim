4abacee68c45485e982ba4cb1ed71ed8f7793164
Phurinut Waranich <phurinut.wara@gmail.com>
2024-07-15 19:47:26 +0700

    fixup! obisdian.lua: for personal note-taking

--- lua/custom/plugins/nvim-essential/obsidian.lua
+++ lua/custom/plugins/nvim-essential/obsidian.lua
@@ -42,6 +42,30 @@ return {
       min_chars = 2,
     },
 
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ['gf'] = {
+        action = function()
+          return require('obsidian').util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ['<leader>ch'] = {
+        action = function()
+          return require('obsidian').util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ['<cr>'] = {
+        action = function()
+          return require('obsidian').util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+
     note_id_func = function(title)
       -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
       -- In this case a note with the title 'My new note' will be given an ID that looks
