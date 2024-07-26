3df507986069d7abec26522b4dd51240b997b6e1
Phurinut Waranich <phurinut.wara@gmail.com>
2024-07-27 01:08:30 +0700

    toggleterm.lua: make it float for ease of use

--- lua/custom/plugins/nvim-essential/toggleterm.lua
+++ lua/custom/plugins/nvim-essential/toggleterm.lua
@@ -5,21 +5,24 @@ return {
       require('toggleterm').setup {
         open_mapping = [[<C-\>]],
         close_on_exit = true,
+        direction = 'float',
       }
 
       local Terminal = require('toggleterm.terminal').Terminal
-      Terminal:new { hidden = true }
+      Terminal:new {
+        hidden = true,
+      }
 
       -- See more: https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#terminal-window-mappings
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
         vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
         -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+        -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        -- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
       end
 
       -- if you only want these mappings for toggle term use term://*toggleterm#* instead
