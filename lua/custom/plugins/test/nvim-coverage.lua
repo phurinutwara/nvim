b0fda58c5961df40c939ae762fe0bd88f888776b
Phurinut Waranich <phurinut.wara@gmail.com>
2024-07-30 15:24:11 +0700

    test: add nvim-coverage and neotest

---
+++ lua/custom/plugins/test/nvim-coverage.lua
@@ -0,0 +1,31 @@
+return {
+  'andythigpen/nvim-coverage',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('coverage').setup {
+      commands = true, -- create commands
+      highlights = {
+        -- customize highlight groups created by the plugin
+        covered = { fg = '#C3E88D' }, -- supports style, fg, bg, sp (see :h highlight-gui)
+        uncovered = { fg = '#F07178' },
+      },
+      signs = {
+        -- use your own highlight groups or text markers
+        covered = { hl = 'CoverageCovered', text = '▎' },
+        uncovered = { hl = 'CoverageUncovered', text = '▎' },
+      },
+      summary = {
+        -- customize the summary pop-up
+        min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+      },
+      lang = {
+        -- customize language specific settings
+      },
+
+      auto_reload = true,
+      load_coverage_cb = function(ftype)
+        vim.notify('Loaded ' .. ftype .. ' coverage')
+      end,
+    }
+  end,
+}
