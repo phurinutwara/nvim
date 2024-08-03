9a6357ae57b18fd59880c47dd392da68c356368f
Phurinut Waranich <phurinut.wara@gmail.com>
2024-08-03 20:16:24 +0700

    toggleterm: add note to exit terminal mode in arch

--- init.lua
+++ init.lua
@@ -185,6 +185,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
+-- In Arch, it is <CTRL_SUPER_\> + <CTRL_SUPER_N>
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
