1c4f815f0ec2b67e7bb1f7ffed74a50fcc170430
Phurinut Waranich <phurinut.wara@gmail.com>
2024-09-21 15:44:45 +0700

    lsp: change tsserver to ts_ls (fix deprecated name)

--- lua/custom/plugins/lsp/init.lua
+++ lua/custom/plugins/lsp/init.lua
@@ -364,8 +364,8 @@ return {
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -403,14 +403,14 @@ return {
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
-            if server_name == 'tsserver' then
-              -- Skip setup for tsserver
+            if server_name == 'ts_ls' then
+              -- Skip setup for ts_ls
               return
             end
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
