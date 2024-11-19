e197f8419e8909e9a3696ab2f6e318a3fd5feb82
Phurinut Waranich <phurinut.wara@gmail.com>
2024-11-19 10:52:58 +0700

    transparent.lua: add transparency toggle

--- lua/custom/plugins/ui/transparent.lua
+++ lua/custom/plugins/ui/transparent.lua
@@ -1,8 +1,17 @@
 return {
   {
     'xiyaowong/transparent.nvim',
+    lazy = false,
     keys = {
       { '<leader>TT', ':TransparentToggle<CR>', desc = 'Transparent Toggle' },
     },
+    configs = function()
+      local transparent = require 'transparent'
+      transparent.setup()
+
+      transparent.clear_prefix 'BufferLine'
+      transparent.clear_prefix 'NeoTree'
+      transparent.clear_prefix 'Lualine'
+    end,
   },
 }
